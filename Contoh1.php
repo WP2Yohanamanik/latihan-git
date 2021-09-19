263f8f2df7b93a13660adf5149141d4a780bedf1
WP2Yohanamanik <90914642+WP2Yohanamanik@users.noreply.github.com>
2021-09-19 12:07:09 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Yohana N Welni Manik 
+            Saya tinggal di daerah Bogor 
+            Olah raga yang saya sukai adalah 
+            bulutangkis";
+
+
+    }
